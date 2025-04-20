8d1f57037456a8cbcd8f943ec9a756e30f61002d
Kassii <kassidy.satterfield@outlook.com>
2025-04-20 18:23:59 -0500

    new fn

--- Accounts.java
+++ Accounts.java
@@ -7,13 +7,13 @@ public class Accounts {
 
 
 
-    public static void MakeAccounts(Scanner in){ //new account
+    public static void MakeAccounts(Scanner in,String username, String password){ //new account
 
         //Scanner user = new Scanner(System.in);
         System.out.println("Please enter your username: ");
         String NewUsername = in.nextLine();
 
-        Scanner password = new Scanner(System.in);
+        //Scanner password = new Scanner(System.in);
         System.out.println("Enter your password: ");
         String NewPassword = in.nextLine();
 
@@ -30,6 +30,24 @@ public class Accounts {
             System.err.println("ERROR: " + e.getMessage());
         }
     }
+
+    public static boolean VerifyAccount(String username, String password){ //dont need to check password bc if one is wrong thats all we need
+        boolean check = true;
+         try(BufferedReader UserRead = new BufferedReader(new FileReader(UserAccounts))){
+
+         }
+         //if account isnt recognized prompt to make a new account
+         //after account is made, call verify again and chck to make sure account exists
+         //loop if still not verified
+
+
+        if(check == true){
+            return true;
+        }
+       else{
+        return false;
+       }
+    }
    
     
 
@@ -38,8 +56,25 @@ public class Accounts {
         System.out.println("Please enter your Email or Username");
         String username = input.nextLine();
 
+        System.out.println("Please enter your password: ");
+        String password = input.nextLine();
+
+        // if(VerifyAccount(username) == true){
+
+        // }
+        // else{
+            
+        //     MakeAccounts(input,username,password);
+        // }
+
+        while(VerifyAccount(username,password)!=true){
+            System.out.println("Sorry, your account doesn't exist. Try again: ")
+
+        }
+
+
         //if else statements
-        MakeAccounts(); //call to make new user account
+        //MakeAccounts(); //call to make new user account
 
 
 
